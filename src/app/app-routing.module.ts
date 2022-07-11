2387e3d2b58f8d60c8ec230902d4f4ee4a2c804d
Liviu Chirea <li.chirea28@gmail.com>
2022-07-11 14:39:22 +0300

    search routerlink works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {SearchResultComponent} from "./search-result/search-result.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "search/:term", component: SearchResultComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
