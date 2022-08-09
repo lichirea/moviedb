b57bfc61d2fbbf172896cf6a8e07928b303343f9
Liviu Chirea <li.chirea28@gmail.com>
2022-08-09 09:17:33 +0300

    no more tests

--- src/app/home/form-test/form-test.component.spec.ts
+++ src/app/home/form-test/form-test.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
 import { FormTestComponent } from './form-test.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
@@ -47,7 +47,84 @@ describe('FormTestComponent', () => {
     expect(movieFormGroup.value).toEqual(movieFormValue);
   });
 
-  it('should update title after input', () => {
-    const movie
-  });
+  //should have set fakeAsync for .whenStable to work.....
+  it('should update title after input', fakeAsync(() => {
+    const movieFormUserElement: HTMLInputElement = fixture.debugElement.nativeElement
+      .querySelector('#movieForm').querySelectorAll('input')[0];
+    movieFormUserElement.value = 'Power of The Dog';
+    movieFormUserElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const titleValueFromGroup = component.movieForm.get('title');
+      expect(movieFormUserElement.value).toEqual(titleValueFromGroup?.value as string);
+      expect(titleValueFromGroup?.errors).toBeNull();
+    })
+    }));
+
+  it('should update original_title after input', fakeAsync(() => {
+    const movieFormUserElement: HTMLInputElement = fixture.debugElement.nativeElement
+      .querySelector('#movieForm').querySelectorAll('input')[1];
+    movieFormUserElement.value = 'Morometii';
+    movieFormUserElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const og_titleValueFromGroup = component.movieForm.get('original_title');
+      expect(movieFormUserElement.value).toEqual(og_titleValueFromGroup?.value as string);
+      expect(og_titleValueFromGroup?.errors).toBeNull();
+    })
+  }));
+
+  it('should update release date after input', fakeAsync(() => {
+    const movieFormUserElement: HTMLInputElement = fixture.debugElement.nativeElement
+      .querySelector('#movieForm').querySelectorAll('input')[2];
+    movieFormUserElement.value = '2005-11-21';
+    movieFormUserElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const release_dateValueFromGroup = component.movieForm.get('release_date');
+      expect(movieFormUserElement.value).toEqual(release_dateValueFromGroup?.value as string);
+      expect(release_dateValueFromGroup?.errors).toBeNull();
+    })
+  }));
+
+  it('should update vote average after input', fakeAsync(() => {
+    const movieFormUserElement: HTMLInputElement = fixture.debugElement.nativeElement
+      .querySelector('#movieForm').querySelectorAll('input')[3];
+    console.log(movieFormUserElement);
+    movieFormUserElement.value = '5';
+    movieFormUserElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const vote_averageValueFromGroup = component.movieForm.get('vote_average');
+      expect(movieFormUserElement.value).toEqual(String(vote_averageValueFromGroup?.value));
+      expect(vote_averageValueFromGroup?.errors).toBeNull();
+    })
+  }));
+
+  it('should update vote count after input', fakeAsync(() => {
+    const movieFormUserElement: HTMLInputElement = fixture.debugElement.nativeElement
+      .querySelector('#movieForm').querySelectorAll('input')[4];
+    movieFormUserElement.value = '1000';
+    movieFormUserElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const vote_countValueFromGroup = component.movieForm.get('vote_count');
+      expect(movieFormUserElement.value).toEqual(String(vote_countValueFromGroup?.value));
+      expect(vote_countValueFromGroup?.errors).toBeNull();
+    })
+  }));
+
+  it('should update adult after input', fakeAsync(() => {
+    const movieFormUserElement: HTMLSelectElement = fixture.debugElement.nativeElement
+      .querySelector('#movieForm').querySelectorAll('select')[0];
+    console.log(movieFormUserElement);
+    movieFormUserElement.value = movieFormUserElement.options[0].value;
+    movieFormUserElement.dispatchEvent(new Event('change'));
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const adultValueFromGroup = component.movieForm.get('adult');
+      expect(movieFormUserElement.value).toEqual(adultValueFromGroup?.value as string);
+      expect(adultValueFromGroup?.errors).toBeNull();
+    })
+  }));
 });
